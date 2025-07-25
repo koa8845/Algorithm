edb44429a597b33466ed491f053f08898231f6f1
“koa8845” <684259@webmail.npru.ac.th>
2025-07-25 16:29:50 +0700

    first commitVariablework

---
+++ Variable.java
@@ -0,0 +1,25 @@
+public class Variables Example {
+    public static void main(String[] args) {
+    
+char clothingSize = 's'; 
+
+char websiteVisitors = '0';
+
+double studentGPA = 'A';
+
+double studentGPA = 4.0; 
+
+final double PT = 3.14159;
+
+float annualExpenses = 15000.00f;
+
+final int MONTHS_TN_YEAR = 12;
+
+System.out.println("Clothing Size: " +clothingSize);
+System.out.println("website Visitors: " + websiteVisitors);
+System.out.println("student Grade: " + student Grade);
+System.out.println("studentGPA" : + studentGPA);
+System.out.println("Value of Pi: "+ PT);
+System.out.println("AnnualExpenses :" + annualExpenses);
+System.out.println("Months in a year :" + MONTHS_TN_YEAR);}
+}
---
+++ birthday.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class age {
+    public void main(String args[])
+    Scanner scanner = new Scanner(system.in);
+system.out.print("Enter num1: ");
+int age = scanner.nextTnt();
+system.out.print("Enter num2: ");
+int year = scanner.nextTnt();
+        int BD = age+year;
+        System.out.println("You were born in ") + BD
+}
