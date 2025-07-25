8fa3752e9ac0cba86f43bcbc588aee58f0434fbe
“koa8845” <684259@webmail.npru.ac.th>
2025-07-25 15:27:04 +0700

    first commitkoa

--- sum.java
+++ sum.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 public class sum {
     public static void main(String args[]){
-        int num1 = 4;
+        
+        // int num1 = 4;
 
-        int num2 = 6;
+        // int num2 = 6;
 
+Scanner scanner = new Scanner(system.in);
+system.out.print("Enter num1: ");
+int num1 = scanner.nextTnt();
+system.out.print("Enter num2: ");
+int num2 = scanner.nextTnt();
         int sum = num1+num2;
 
     System.out.println("sum of"+num1+"+"+num2+"="+sum);
